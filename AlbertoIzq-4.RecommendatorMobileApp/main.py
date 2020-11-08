9961582b6668c95f21755330c000721fadb7e0b4
Alberto <alberto.izquierdomo@gmail.com>
2020-11-08 22:49:13 +0100

    Create kivy files structure

--- AlbertoIzq-4.RecommendatorMobileApp/main.py
+++ AlbertoIzq-4.RecommendatorMobileApp/main.py
@@ -2,10 +2,57 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('design.kv') # To connect it to kv file
+Builder.load_file('design.kv')
+Builder.load_file('kivy/login.kv')
+Builder.load_file('kivy/sign_up.kv')
+Builder.load_file('kivy/sign_up_success.kv')
+Builder.load_file('kivy/forget_pass.kv')
+Builder.load_file('kivy/forget_pass_success.kv')
+Builder.load_file('kivy/main.kv')
+Builder.load_file('kivy/music.kv')
+Builder.load_file('kivy/movies.kv')
+Builder.load_file('kivy/series.kv')
+Builder.load_file('kivy/books.kv')
+Builder.load_file('kivy/places.kv')
+Builder.load_file('kivy/other.kv')
 
 class LoginScreen(Screen):
     pass
+
+class SignUpScreen(Screen):
+    pass
+
+class SignUpScreenSuccess(Screen):
+    pass
+
+class ForgetPassScreen(Screen):
+    pass
+
+class ForgetPassScreenSuccess(Screen):
+    pass
+
+
+class Main(Screen):
+    pass
+
+class Music(Screen):
+    pass
+
+class Movies(Screen):
+    pass
+
+class Series(Screen):
+    pass
+
+class Books(Screen):
+    pass
+
+class Places(Screen):
+    pass
+
+class Other(Screen):
+    pass
+
 
 class RootWidget(ScreenManager):
     pass
